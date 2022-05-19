1faa9a0070c6c93e26153432c863c945f81054d7
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-05-19 21:25:01 +0530

    Users and their profiles

---
+++ database/migrations/2022_05_19_153333_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('userId')->nullable(false);
+            $table->string('title', 255)->nullable(false);
+            $table->string('content', 3000)->nullable(false);
+            $table->timestamps();
+
+            $table->index('userId');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
