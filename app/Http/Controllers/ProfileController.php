601c3975221ce69ecfb545e243e164b941dbaa2b
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-05-19 21:04:22 +0530

    Add Profile page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function index($user) {
+        $user = User::find($user);
+
+        return view('profile', [
+            'user' => $user
+        ]);
+    }
 }
